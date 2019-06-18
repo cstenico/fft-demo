0c3ae68dfdbced0d2718d92a724c81324fb18847
camila <k.stenico@gmail.com>
2019-06-18 17:20:49 -0300

    it works kkkkk

--- music/music.ino
+++ music/music.ino
@@ -1,5 +1,9 @@
 
+int incomingByte = 0;  
+
 //=== Pin Assignment ===//
+
+char payload[150];
 
 const int DO = 6; // Digital PIN 2 of Arduino
 
@@ -18,13 +22,13 @@ const int led_07 = 13;
  * The values has 4 gap values, you can make how many gaps you like.
  */
  
-int sVal_01 = 500; // Initial Threshold
-int sVal_02 = 510;
-int sVal_03 = 510;
-int sVal_04 = 520;
-int sVal_05 = 520;
-int sVal_06 = 530;
-int sVal_07 = 530;
+int sVal_01 = 50; // Initial Threshold
+int sVal_02 = 70;
+int sVal_03 = 80;
+int sVal_04 = 90;
+int sVal_05 = 100;
+int sVal_06 = 120;
+int sVal_07 = 155;
 
 
 int lastPin = 13; // last digital PIN number
@@ -34,7 +38,7 @@ int totalLED = 7; // total LEDs
  * Additional Settings
  */
  
-int debugSwitch = true; // Switch for Debugging: true or false (ctrl+shift+M or ctrl+shift+L)
+int debugSwitch = false; // Switch for Debugging: true or false (ctrl+shift+M or ctrl+shift+L)
 int ledTest = true; // If true it will test the LEDs first before use (you can press reset to check LEDs if running correctly
 
 /**
@@ -43,12 +47,8 @@ int ledTest = true; // If true it will test the LEDs first before use (you can p
  */
 
 int sensorvalue = 0; // storage for recording sensor values
- 
 void setup() {
-  Serial.begin(9600);
-  for (int i = 6; i <= lastPin; i++) {
-    pinMode(i, OUTPUT);
-  }
+  Serial.begin(115200); // use the same baud-rate as the python side
 
   if (ledTest == true) {
     initTest(); // Initial testing for LED's
@@ -57,10 +57,25 @@ void setup() {
 
 void loop() {
   //sensorvalue = analogRead(DA); // getting sensor values from analog A0
+
+  char data[2];
   if (debugSwitch == true) {
     debug();
   } else {
-    Serial.println("The debugging mode is set to false!");
+    //Serial.println("The debugging mode is set to false!");
+  }
+
+  if (Serial.available() >0) {
+    //char c = Serial.read();
+    //data[0] = c;
+    //data[1] = '\0';
+    //Serial.println(data);
+    incomingByte = Serial.read(); 
+    sensorvalue = incomingByte ;
+
+                // say what you got:
+                Serial.print("I received: ");
+                Serial.println(incomingByte);
   }
 
   // Lights Animation
@@ -88,16 +103,12 @@ void initTest() {
   digitalWrite(led_07, HIGH);
   delay(1000);
   digitalWrite(led_02, HIGH);
-  digitalWrite(led_08, HIGH);
   delay(1000);
   digitalWrite(led_01, HIGH);
-  digitalWrite(led_09, HIGH);
   delay(2000);
   digitalWrite(led_01, LOW);
-  digitalWrite(led_09, LOW);
   delay(500);
   digitalWrite(led_02, LOW);
-  digitalWrite(led_08, LOW);
   delay(500);
   digitalWrite(led_03, LOW);
   digitalWrite(led_07, LOW);
@@ -112,46 +123,78 @@ void music() {
   
   // Basically the LED will turn on if the sensorvalue reach the threshold
   
-  if(sensorvalue >= sVal_01) {
-    digitalWrite(led_05, HIGH);
-  } else {
-    digitalWrite(led_05, LOW);
-  }
-
-  if(sensorvalue >= sVal_02) {
-    digitalWrite(led_04, HIGH);
-  } else {
-    digitalWrite(led_04, LOW);
-  }
-
-  if(sensorvalue >= sVal_03) {
-    digitalWrite(led_06, HIGH);
-  } else {
-    digitalWrite(led_06, LOW);
-  }
-
-  if(sensorvalue >= sVal_04) {
-    digitalWrite(led_03, HIGH);
-  } else {
-    digitalWrite(led_03, LOW);
-  }
-
-  if(sensorvalue >= sVal_05) {
-    digitalWrite(led_07, HIGH);
-  } else {
-    digitalWrite(led_07, LOW);
-  }
-
-  if(sensorvalue >= sVal_06) {
-    digitalWrite(led_02, HIGH);
-  } else {
-    digitalWrite(led_02, LOW);
-  }
-
-
-
-  if(sensorvalue >= sVal_08) {
-    digitalWrite(led_01, HIGH);
-  } else {
+  if(sensorvalue == 0) {
     digitalWrite(led_01, LOW);
-  }
+    digitalWrite(led_02, LOW);
+    digitalWrite(led_03, LOW);
+    digitalWrite(led_04, LOW);
+    digitalWrite(led_05, LOW);
+    digitalWrite(led_06, LOW);
+    digitalWrite(led_07, LOW);
+  }else
+
+    if(sensorvalue == 1) {
+    digitalWrite(led_01, HIGH);
+    digitalWrite(led_02, LOW);
+    digitalWrite(led_03, LOW);
+    digitalWrite(led_04, LOW);
+    digitalWrite(led_05, LOW);
+    digitalWrite(led_06, LOW);
+    digitalWrite(led_07, LOW);
+    }else
+    if(sensorvalue == 2) {
+    digitalWrite(led_01, HIGH);
+    digitalWrite(led_02, HIGH);
+    digitalWrite(led_03, LOW);
+    digitalWrite(led_04, LOW);
+    digitalWrite(led_05, LOW);
+    digitalWrite(led_06, LOW);
+    digitalWrite(led_07, LOW);
+    }else
+    if(sensorvalue == 3) {
+    digitalWrite(led_01, HIGH);
+    digitalWrite(led_02, HIGH);
+    digitalWrite(led_03, HIGH);
+    digitalWrite(led_04, LOW);
+    digitalWrite(led_05, LOW);
+    digitalWrite(led_06, LOW);
+    digitalWrite(led_07, LOW);
+    }else
+    if(sensorvalue == 4) {
+    digitalWrite(led_01, HIGH);
+    digitalWrite(led_02, HIGH);
+    digitalWrite(led_03, HIGH);
+    digitalWrite(led_04, HIGH);
+    digitalWrite(led_05, LOW);
+    digitalWrite(led_06, LOW);
+    digitalWrite(led_07, LOW);
+    }else
+    if(sensorvalue == 5) {
+    digitalWrite(led_01, HIGH);
+    digitalWrite(led_02, HIGH);
+    digitalWrite(led_03, HIGH);
+    digitalWrite(led_04, HIGH);
+    digitalWrite(led_05, HIGH);
+    digitalWrite(led_06, LOW);
+    digitalWrite(led_07, LOW);
+    }else
+    if(sensorvalue == 6) {
+    digitalWrite(led_01, HIGH);
+    digitalWrite(led_02, HIGH);
+    digitalWrite(led_03, HIGH);
+    digitalWrite(led_04, HIGH);
+    digitalWrite(led_05, HIGH);
+    digitalWrite(led_06, HIGH);
+    digitalWrite(led_07, LOW);
+    }
+    if(sensorvalue == 7) {
+    digitalWrite(led_01, HIGH);
+    digitalWrite(led_02, HIGH);
+    digitalWrite(led_03, HIGH);
+    digitalWrite(led_04, HIGH);
+    digitalWrite(led_05, HIGH);
+    digitalWrite(led_06, HIGH);
+    digitalWrite(led_07, HIGH);
+    }
+
+}
--- py/audio.py
+++ py/audio.py
@@ -1,6 +1,12 @@
 import pyaudio
 import numpy as np
 import matplotlib.pyplot as plt
+import serial
+import time
+import struct
+arduino = serial.Serial('/dev/cu.usbmodem14101', 115200, timeout=.1)
+
+time.sleep(1)
 
 np.set_printoptions(suppress=True) # don't use scientific notation
 
@@ -21,6 +27,37 @@ while True: #to it a few times just to see
     freq = freq[:int(len(freq)/2)] # keep only first half
     freqPeak = freq[np.where(fft==np.max(fft))[0][0]]+1
     print("peak frequency: %d Hz"%freqPeak)
+    if (freqPeak > 5000 and freqPeak <= 800):
+        print(1)
+        arduino.write(struct.pack('>B', 1))
+        #arduino.write(str(1))
+    elif (freqPeak > 800 and freqPeak <= 1000):
+        print(2)
+        arduino.write(struct.pack('>B', 2))
+        #arduino.write(str(2))
+    elif (freqPeak > 1000 and freqPeak <= 1100):
+        print(3)
+        arduino.write(struct.pack('>B', 3))
+        #arduino.write(str(3))
+    elif (freqPeak > 1100 and freqPeak <= 1150):
+        print(4)
+        arduino.write(struct.pack('>B', 4))
+        #arduino.write(str(4))
+    elif (freqPeak > 1150 and freqPeak <= 1200):
+        print(5)
+        arduino.write(struct.pack('>B', 5))
+        #arduino.write(str(5))
+    elif (freqPeak > 1200 and freqPeak <= 1350):
+        print(6)
+        arduino.write(struct.pack('>B', 6))
+        #arduino.write(str(6))
+    elif (freqPeak > 1350):
+        print(7)
+        arduino.write(struct.pack('>B', 7))
+        #arduino.write(str(7))
+    else:
+        arduino.write(struct.pack('>B', 0))
+        #arduino.write(str(0))
 
     # uncomment this if you want to see what the freq vs FFT looks like
     #plt.plot(freq,fft)
